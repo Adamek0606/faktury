ff363489211b6d5f8c9a304ac50d32edb9a2e0e4
Adamek0606 <adamek06adamek@gmail.com>
2025-11-05 18:10:55 +0100

    1.0

---
+++ src/main/java/adam/sowinski/pl/invoice/App.java
@@ -0,0 +1,13 @@
+package adam.sowinski.pl.invoice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
